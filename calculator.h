2ba9cee62dcc0cc67c46e717a1e56ebf84b6d8f6
biriusthebest <55427106+biriusthebest@users.noreply.github.com>
2020-09-29 00:24:37 +0300

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+        int Add (int, int);
+        int Sub (double, double);
+        int Mul (double, double);
+};
+
+#endif//CALCULATOR_H
